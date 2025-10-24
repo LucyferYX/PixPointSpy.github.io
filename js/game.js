44eafa8a2af0171f520461c8153547c0cead8d81
Liene <liene.krista@gmail.com>
2025-10-25 00:16:09 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -430,7 +430,7 @@ function surrenderGame() {
         ctx.imageSmoothingEnabled = false;
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-        ctx.lineWidth = 1;
+        ctx.lineWidth = 2;
         ctx.strokeStyle = 'red';
         ctx.strokeRect(
             gridInfo.x * cellWidth - 1,
